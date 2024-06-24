070c1e0b2901d7f759065dc554a753cf308ceaf2
Emerson Oliveira - DS <114004242+Emerson-C-Oliveira@users.noreply.github.com>
2024-06-24 17:38:00 -0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(
+    page_title="Home",
+    page_icon=" ",
+    layout="wide"
+)
+
+# image_path = 'C:/Users/emers/Documents/repos/ftc_programacao_python/notebooks/'
+image_path = '/home/emersds/repos_projetos/curry_company/capa_currycompany.png'
+image = Image.open(image_path)
+st.sidebar.image(image, use_column_width=True)
+
+st.sidebar.markdown('# Cury Company')
+st.sidebar.markdown('## A entrega mais rápida do país')
+st.sidebar.markdown("""___""")
+
+st.write('# Curry Company Growth Dashboard')
+st.markdown(
+"""
+   Growth dashboard foi construído para acompanhar as métricas de crescimento dos Entregadores e Restaurantes.
+   ### Como utilizar esse Growth Dashboard?
+   - Visão empresa:
+     - Visão gerencial: métricas gerais de comportamento.
+     - Visão tática: indicadores semanais de crescimento.
+     - Visão geográfica: insights de geolocalização.
+
+   - Visão entragadores:
+     - Acompanhamento dos indicadores semanais de crescimento.
+
+   - Visão restaurantes:
+     - Indicadores semanais de crescimento dos restaurantes.
+
+   ### Ask for Help (Contacts)
+   - Time de Data Science
+       - Discord: @emersoncoliveira
+       - https://sites.google.com/view/emerson-oliveira-portfolio/
+       - https://www.linkedin.com/in/emerson-carlos-oliveira
+       - https://emerson-c-oliveira.github.io/portfolio_project_ds/#
+""")
